1cd7e2e538edbfa7bb4c6b2eb312148e0190b6d6
Aseelwadi <aseelalsqoor23@gmail.com>
2021-03-30 09:36:25 -0700

    lab3

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 'use strict';
 
-var valueOption = [];
-var optionTwo = [];
+var optionOne = []; //to get page 1 ajax .
+var optionTwo = []; //to get page 1 ajax .
 
-var uniqueOptionValue = [];
-var uniqueOptionValueTwo = [];
+var optionValue = []; //sort pages array by title
+var optionValueTwo = []; //sort pages array by horns
 
-var arrayImages = [];
-var arrayImagesTwo = [];
+var arrayImages = []; //page-1 images
+var arrayImagesTwo = []; //page-2 images
 
 
-//age-1 images 
+//get page-1 images 
 function Images(title, image_url, description, horns, keyword) {
     this.title = title;
     this.image_url = image_url;
@@ -34,12 +34,12 @@ function ImagesTwo(title, image_url, description, horns, keyword) {
 //render images 1
 Images.prototype.renderImages = function() {
         let temp = $('#template-img').html();
-        $('.main-1').append(Mustache.render(temp, this));
+        $('.main-a').append(Mustache.render(temp, this));
     }
     //render images 2
 ImagesTwo.prototype.renderImages2 = function() {
     let temp = $('#template-img').html();
-    $('.main-2').append(Mustache.render(temp, this));
+    $('.main-b').append(Mustache.render(temp, this));
 }
 
 
@@ -53,15 +53,15 @@ Images.readJson = () => {
     $.ajax('data/page-1.json', ajaxSettings)
         .then(data => {
             data.forEach(element => {
-                valueOption.push(element.keyword);
+                optionOne.push(element.keyword);
                 let image = new Images(element.title, element.image_url, element.description, element.horns, element.keyword);
                 image.renderImages();
             });
-            $.each(valueOption, function(i, el) {
-                if ($.inArray(el, uniqueOptionValue) === -1) uniqueOptionValue.push(el);
+            $.each(optionOne, function(i, el) {
+                if ($.inArray(el, optionValue) === -1) optionValue.push(el);
             });
 
-            uniqueOptionValue.forEach(function(value, i) {
+            optionValue.forEach(function(value, i) {
                 $("#menu").append("<option value=" + value + ">" + value + "</option>");
             });
         });
@@ -81,10 +81,10 @@ ImagesTwo.readJson2 = () => {
 
             });
             $.each(optionTwo, function(i, el) {
-                if ($.inArray(el, uniqueOptionValueTwo) === -1) uniqueOptionValueTwo.push(el);
+                if ($.inArray(el, optionValueTwo) === -1) optionValueTwo.push(el);
             });
 
-            uniqueOptionValueTwo.forEach(function(value, i) {
+            optionValueTwo.forEach(function(value, i) {
                 $("#menu").append("<option value=" + value + ">" + value + "</option>");
             });
         });
@@ -94,8 +94,8 @@ function selectKeywords() {
     $("#menu").on('change', function() {
         var name = this.options[this.selectedIndex].text;
         var newPhotoTemplate2 = $('.photo-template').clone();
-        $('.main-1').html("");
-        $('.main-1').append(newPhotoTemplate2);
+        $('.main-a').html("");
+        $('.main-a').append(newPhotoTemplate2);
         arrayImages.forEach(function(value, i) {
             if (name === value.keyword) {
                 value.renderImages();
@@ -108,8 +108,8 @@ function selectKeywordsTwo() {
     $("#menu").on('change', function() {
         var name = this.options[this.selectedIndex].text;
         var newPhotoTemplateTwo = $('#template-img').clone();
-        $('.main-2').html("");
-        $('.main-2').append(newPhotoTemplateTwo);
+        $('.main-b').html("");
+        $('.main-b').append(newPhotoTemplateTwo);
         arrayImagesTwo.forEach(function(value, i) {
             if (name === value.keyword) {
                 value.renderImages2();
@@ -127,11 +127,11 @@ $("document").ready(function() {
 
 var pageName = "page-1";
 $("#page-1").on("click", function() {
-    // $('.photo-template').html("");
+
     $("#menu").html("");
-    $(".main-2").hide();
-    $(".main-1").show();
-    $(".main-1").empty();
+    $(".main-b").hide();
+    $(".main-a").show();
+    $(".main-a").empty();
     Images.readJson();
     selectKeywords();
     pageName = "page-1";
@@ -140,11 +140,11 @@ $("#page-1").on("click", function() {
 });
 
 $("#page-2").on("click", function() {
-    // $(".photo-template2").html("");
+
     $("#menu").html("");
-    $(".main-1").hide();
-    $(".main-2").show();
-    $(".main-2").empty();
+    $(".main-a").hide();
+    $(".main-b").show();
+    $(".main-b").empty();
     ImagesTwo.readJson2();
     selectKeywordsTwo();
     pageName = "page-2";
@@ -170,7 +170,7 @@ function sortOnTitle() {
             return 0;
         });
 
-        $('.main-1').html("");
+        $('.main-a').html("");
         arrayImages.forEach(function(value, i) {
             value.renderImages();
         });
@@ -183,7 +183,7 @@ function sortOnTitle() {
             return 0;
         });
 
-        $('.main-2').html("");
+        $('.main-b').html("");
         arrayImagesTwo.forEach(function(value, i) {
             value.renderImages2();
         });
@@ -200,7 +200,7 @@ function sortOnHorns() {
             return 0;
         });
 
-        $('.main-1').html("");
+        $('.main-a').html("");
         arrayImages.forEach(function(value, i) {
             value.renderImages();
         });
@@ -213,7 +213,7 @@ function sortOnHorns() {
             return 0;
         });
 
-        $('.main-2').html("");
+        $('.main-b').html("");
         arrayImagesTwo.forEach(function(value, i) {
             value.renderImages2();
         });
